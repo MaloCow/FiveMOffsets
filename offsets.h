7427867b810f3ad3149b9702d10c303e9272eff1
MaloCow <68386674+MaloCow@users.noreply.github.com>
2022-03-24 14:43:50 +0100

    Create offsets.h

---
+++ offsets.h
@@ -0,0 +1,23 @@
+// Network	
+uintptr_t netInfo = 0x1C150;
+uintptr_t portInfo = 0x1C490;
+
+// Resources
+uintptr_t GetName = 0x1A6F0;
+uintptr_t GetPath = 0x1A720;
+uintptr_t GetState = 0x1A730;
+uintptr_t ForAll = 0x24F50;
+uintptr_t Stop = 0x1AD40;
+uintptr_t Start = 0x1AC80;
+
+// CitizenScriptingLua
+uintptr_t currentLuaRuntime = 0xE71F8;
+uintptr_t LoadFile = 0x1B490;
+uintptr_t DoFile = 0x2FA50;
+
+// Player(s)
+uintptr_t HealthOffset = 0x280;
+uintptr_t GodOffset = 0x189;
+uintptr_t MaxHealthOffset = 0x2A0;
+uintptr_t ArmorOffset = 0x14B8;
+uintptr_t PlayerOffset = 0x8;
